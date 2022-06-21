f506c61dd3e86345eac191bb9325d84dc38f703b
justmarous <mmbilnicki@gmail.com>
2022-06-21 16:16:11 +0200

    heroku deploy soon

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import Title from "./components/Title";
-// import Colours from "./components/Colours";
-// import Numbers from "./components/Numbers";
-// import Card from "./components/Card";
-// import {selectCard, selectNumber} from "./redux/drawerSlice";
+import Colours from "./components/Colours";
+import Numbers from "./components/Numbers";
 import Footer from "./components/Footer";
 import store from "./redux/store";
-import {Provider} from "react-redux";
+import {Provider, useSelector} from "react-redux";
+import Card from "./components/Card";
+import {selectCard, selectNumber} from "./redux/drawerSlice";
 import CardPicker from "./components/CardPicker";
-import express from "express"
-const app = express();
-
-const path = require('path');
-const port = process.env.PORT || 5000
-
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'build','index.html'))
-    })
-}
-
-app.listen(port, (err) => {
-    if (err) return console.log(err);
-    console.log('Server running on port: ',port)
-})
 
 
 function App() {
